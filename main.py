1e04a4b3746257ca834c70f212cdd53d1da310bb
foxx <jkoontsiii@gmail.com>
2022-06-30 20:57:40 -0400

    .

--- main.py
+++ main.py
@@ -3,26 +3,16 @@ import os
 import time
 import datetime
 import discordwh
+import opengarage
 
 dtime = int(time.time())
 ctime = int(time.time())
 
 james = 1
-koonts = 1
+ktz = 1
 jk1 = ctime
 jk2 = dtime
 jkhost = "10.0.0.203"
-
-matt = 1
-bell = 1
-mb1 = ctime
-mb2 = dtime
-mbhost = "10.0.0.212"
-
-#tom = 1
-#mac = 1
-#tm1 = ctime
-#tm2 = dtime
 
 
 # 300 for 5 mins
@@ -41,17 +31,13 @@ def printtime():
 
 def check_host(x):
 	hostname = x
-	response = os.system("ping -c 1 " + hostname + ">/dev/null 2>&1")
+	response = os.system("ping -c1 -w2 " + hostname + ">/dev/null 2>&1")
 	if response != 0:
 		up = 0
 	# and then check the response...
 	elif response == 0:
 		up = 1
 	return up
-
-
-def open_garage():
-	os.system("python3 /home/foxx/.scripts/open-koonts-garage.py")
 
 
 def runcheck(host, c2, c1, ct, dt):
@@ -68,19 +54,17 @@ def runcheck(host, c2, c1, ct, dt):
 			if timepassage(ct, dt, timethres):
 				print("opening garage")
 				message = "Opening Garage: " + str(cc) + " of " + str(timethres)
-				discordwh.discord_notif(message)
+				#discordwh.discord_notif(message)
 				printtime()
-				open_garage()
+				opengarage.opengarage()
 				print("---------------------")
 			else:
 				print("15 mins has not passed")
-				#print("opening garage")
 				cc = ct - dt
 				message = "Time Threshold not reached: " + str(cc) + " of " + str(timethres)
-				discordwh.discord_notif(message)
+				#discordwh.discord_notif(message)
 				print("Time passed:", ct - dt, " of", timethres)
 				printtime()
-				#open_garage()
 				print("---------------------")
 	elif c1 != c2 and c2 == 0:
 		print("---------------------")
@@ -98,5 +82,4 @@ def runcheck(host, c2, c1, ct, dt):
 
 
 while True:
-	jkhost,james,koonts,jk1,jk2 = runcheck(jkhost, james, koonts, jk1, jk2)
-	mbhost,matt,bell,mb1,mb2 = runcheck(mbhost, matt, bell, mb1, mb2)
+	jkhost,james,ktz,jk1,jk2 = runcheck(jkhost, james, ktz, jk1, jk2)
